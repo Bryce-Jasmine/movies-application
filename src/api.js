5f07640fe7e74b341e3e12cfb273baa746d37a41
Bryce Payne <brycedpayne@gmail.com>
2020-03-13 15:33:48 -0500

    your turn

--- src/api.js
+++ src/api.js
@@ -1,22 +1,49 @@
 const $ = require("jquery");
-module.exports = {
-  getMovies: () => {
-    return fetch('/api/movies')
-      .then(response => response.json());
 
-  },
-  addMovie:(e) => {
-    e.preventDefault();
-    const newMovie = {title: $("#title").val(), rating: $("#rating").val()};
-    console.log(newMovie);
-    const url = "/api/movies";
-    const options = {
-      method: "POST",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(newMovie)
-    };
-    return fetch(url, options)
-        .then()
-        .catch()
-  }
+module.exports = {
+    getMovies: () => {
+        return fetch('/api/movies')
+            .then(response => response.json());
+
+    },
+    addMovie: (e) => {
+        e.preventDefault();
+        const newMovie = {title: $("#title").val(), rating: $("#rating").val()};
+        console.log(newMovie);
+        const url = "/api/movies";
+        const options = {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(newMovie)
+        };
+        return fetch(url, options)
+            .then()
+            .catch()
+    },
+    getEditMovie: () => {
+        let index = $('#idEdit').val();
+        return fetch(`/api/movies/${index}`)
+            .then(response => response.json());
+    },
+    editMovie: (id, movie) => {
+        return fetch(`api/movies/${id}`, {
+            method: 'Patch',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(movie)
+        })
+            .then(response => response.json());
+    },
+    // editMovie: () => {
+    //     let index = $('#idEdit').val();
+    //     console.log(index);
+    //     let movieObj = fetch('/api/movies/')
+    //         .then(response => response.json())
+    //         .then()
+    //     console.log(movieObj);
+    //
+    //
+    // }
+
+    // nothing important
+
 };
--- src/index.js
+++ src/index.js
@@ -2,27 +2,47 @@
  * es6 modules and imports
  */
 import sayHello from './hello';
+
 sayHello('World');
 
 /**
  * require style imports
  */
-const {getMovies, addMovie} = require('./api.js');
+const {getMovies, addMovie, editMovie, getEditMovie} = require('./api.js');
 const $ = require("jquery");
 
 getMovies().then((movies) => {
-  $("#movies").html('Here are all the movies:');
-  let list = "<ul>";
-  movies.forEach(({title, rating, id}) => {
-    list += (`<li> id#${id} - ${title} - rating: ${rating} </li>`);
-    // $("#moviesOutput").html(`id#${id} - ${title} - rating: ${rating}`);
-  });
-  list += "</ul>";
-  $("#moviesOutput").html(list);
+    $("#movies").html('Here are all the movies:');
+    let list = "<ul>";
+    movies.forEach(({title, rating, id}) => {
+        list += (`<li>${id}.)  <strong>${title}</strong>, Rating: ${rating} </li>`);
+        // $("#moviesOutput").html(`id#${id} - ${title} - rating: ${rating}`);
+    });
+    list += "</ul>";
+    $("#moviesOutput").html(list);
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.');
-  console.log(error);
+    alert('Oh no! Something went wrong.\nCheck the console for details.');
+    console.log(error);
 });
+
+// getEditMovie().then((movie) => {
+//     console.log(movie);
+// })
+
 
 $("#addMovie").click(addMovie);
 $("#refresh").click(getMovies);
+$('#this-must-work').click(function () {
+    console.log('hello im working')
+    getEditMovie()
+        .then((data) => {$('#titleEdit').val(data.title);$('#ratingEdit').val(data.rating)});
+});
+
+$('#editMovie').click(function (){
+    editMovie($('#titleEdit').val(),$('#ratingEdit').val());
+})
+// console.log(getEditMovie());
+// .then((movie) => {
+//     console.log(movie.title);
+//     $('#ratingEdit').val(movie.rating)
+// }));
