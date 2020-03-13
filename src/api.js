1d6d0c07c74c7480d5320844da7eac714f2894b5
Jasmine Rivera <jasmineannrivera@gmail.com>
2020-03-13 12:01:45 -0500

    base functionality achieved yay

--- src/api.js
+++ src/api.js
@@ -1,6 +1,22 @@
+const $ = require("jquery");
 module.exports = {
   getMovies: () => {
     return fetch('/api/movies')
       .then(response => response.json());
+
+  },
+  addMovie:(e) => {
+    e.preventDefault();
+    const newMovie = {title: $("#title").val(), rating: $("#rating").val()};
+    console.log(newMovie);
+    const url = "/api/movies";
+    const options = {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(newMovie)
+    };
+    return fetch(url, options)
+        .then()
+        .catch()
   }
 };
--- src/index.js
+++ src/index.js
@@ -7,14 +7,22 @@ sayHello('World');
 /**
  * require style imports
  */
-const {getMovies} = require('./api.js');
+const {getMovies, addMovie} = require('./api.js');
+const $ = require("jquery");
 
 getMovies().then((movies) => {
-  console.log('Here are all the movies:');
+  $("#movies").html('Here are all the movies:');
+  let list = "<ul>";
   movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+    list += (`<li> id#${id} - ${title} - rating: ${rating} </li>`);
+    // $("#moviesOutput").html(`id#${id} - ${title} - rating: ${rating}`);
   });
+  list += "</ul>";
+  $("#moviesOutput").html(list);
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
+  alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
 });
+
+$("#addMovie").click(addMovie);
+$("#refresh").click(getMovies);
