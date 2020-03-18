383d2080057dc327ad2c427226fd314484ff0049
Bryce Payne <brycedpayne@gmail.com>
2020-03-18 15:20:30 -0500

    added sort and search functions

--- src/index.js
+++ src/index.js
@@ -22,13 +22,11 @@ function refreshMovies() {
     loadMovies();
 }
 
-// get movies function that populates the .json
-function loadMovies() {
-    getMovies().then((movies) => {
-        $("#movies").html(/*'Here are all the movies:'*/'');
-        let list = "";  //<div class='movies container'>
-        movies.forEach(({title, genre, rating, id}) => {
-            list += (`<div class = "movies" id="${id}"> 
+function populate(arr) {
+    $("#movies").html(/*'Here are all the movies:'*/'');
+    let list = "";  //<div class='movies container'>
+    arr.forEach(({title, genre, rating, id}) => {
+        list += (`<div class = "movies" id="${id}"> 
                  <h5>${title}</h5>
                  <div class="d-flex justify-content-between align-items-baseline">
                  <p style="width: 130px">Genre: ${genre}</p> <p>Rating: ${rating}</p>
@@ -43,47 +41,55 @@ function loadMovies() {
                     </div>
                     <hr>
              </div>`);
-            $("#moviesOutput").html(`id#${id} - ${title} - rating: ${rating}`);
-        });
-        // list += "</div>";
-        $("#moviesOutput").html(list);
-        // click event listener that deletes movie from database
-        $(".deleteMovie").click((evt) => getDeleteMovie(evt).then(() => refreshMovies()));
-        // click event listener that edits movie in database
-        $('#editMovie').click(function (evt) {
-            // console.log("test");
-            let id = $(evt.target).siblings('#idEdit')[0].value;                                    //var for id value
-            let movie = {
-                title: $('#titleEdit').val(),
-                genre: $('#genreEdit').val(),
-                rating: $('#ratingEdit').val()
-            };     //var for info being edited
-
-            editMovie(id, movie).then(loadMovies)         //execute the edit movie function
-                .then(() => {
-                    $('#titleEdit').val('');
-                    $('#genreEdit').val('');
-                    $('#ratingEdit').val('');
-                    document.getElementById('editMovie').disabled = true;
-                })
-        });
-        // click event listener that picks the movie to edit based on the value of the id field
-        $('.getEditMovie').click(function (evt) {
-            // console.log('hello im working'); //working/loading message.
-            getEditMovie(evt)          //movie to edit picker
-                .then((data) => {
-                    $('#titleEdit').val(data.title);
-                    $("#genreEdit").val(data.genre);
-                    $('#ratingEdit').val(data.rating);
-                    $('#idEdit').val(data.id);
-                })  // result from the promise resolution
-                .then(enableEditBtn);
-        });
-
-    }).catch((error) => {
-        alert('Oh no! Something went wrong.\nCheck the console for details.');
-        console.log(error);
+        $("#moviesOutput").html(`id#${id} - ${title} - rating: ${rating}`);
     });
+    $("#moviesOutput").html(list);
+    // click event listener that deletes movie from database
+    $(".deleteMovie").click((evt) => getDeleteMovie(evt).then(() => refreshMovies()));
+    // click event listener that edits movie in database
+    $('#editMovie').click(function (evt) {
+        // console.log("test");
+        let id = $(evt.target).siblings('#idEdit')[0].value;                                    //var for id value
+        let movie = {
+            title: $('#titleEdit').val(),
+            genre: $('#genreEdit').val(),
+            rating: $('#ratingEdit').val()
+        };     //var for info being edited
+
+        editMovie(id, movie).then(loadMovies)         //execute the edit movie function
+            .then(() => {
+                $('#titleEdit').val('');
+                $('#genreEdit').val('');
+                $('#ratingEdit').val('');
+                document.getElementById('editMovie').disabled = true;
+            })
+    });
+    // click event listener that picks the movie to edit based on the value of the id field
+    $('.getEditMovie').click(function (evt) {
+        getEditMovie(evt)          //movie to edit picker
+            .then((data) => {
+                $('#titleEdit').val(data.title);
+                $("#genreEdit").val(data.genre);
+                $('#ratingEdit').val(data.rating);
+                $('#idEdit').val(data.id);
+            })  // result from the promise resolution
+            .then(enableEditBtn);
+    });
+}
+
+// get movies function that populates the .json
+let sortArr = [];
+
+function loadMovies() {
+    getMovies()
+        .then((movies) => {
+            sortArr = movies;
+            populate(movies);
+        })
+        .catch((error) => {
+            alert('Oh no! Something went wrong.\nCheck the console for details.');
+            console.log(error);
+        });
 }
 
 // click event listener to add a new movie
@@ -101,15 +107,76 @@ function enableAddBtn() {
 }
 
 function enableEditBtn() {
-    if ($('#titleEdit').val() !== '' && $('#ratingEdit').val() !== '') {
-        document.getElementById('editMovie').disabled = false;
-    } else {
-        document.getElementById('editMovie').disabled = true;
-    }
+    document.getElementById('editMovie').disabled = !($('#titleEdit').val() !== '' && $('#ratingEdit').val() !== '');
 }
 
 $('#newMovie input').blur(enableAddBtn);
 
+// sort functionality
+$("#sort").click(function () {
+    let method = $('#sortSelect').val();
+    console.log(method);
 
-// click event listener that grabs movie to delete
-// $(".deleteMovie").click(getDeleteMovie);
+    if (method.toLowerCase() === "title ascending") {
+        populate(sortArr.sort(compareTitle));
+    } else if (method.toLowerCase() === "genre descending") {
+        populate(sortArr.sort(compareGenre).reverse());
+    } else if (method.toLowerCase() === "rating ascending") {
+        populate(sortArr.sort(compareRating));
+    } else if (method.toLowerCase() === "title descending") {
+        populate(sortArr.sort(compareTitle).reverse());
+    } else if (method.toLowerCase() === "genre ascending") {
+        populate(sortArr.sort(compareGenre));
+    } else if (method.toLowerCase() === "rating descending") {
+        populate(sortArr.sort(compareRating).reverse());
+    }
+});
+
+$('#search').click(function (){
+    let comp = $('#searchBar').val();
+    let myName = sortArr.filter(movie => movie.title.includes(comp));
+    populate(myName);
+});
+// sort comparator function
+function compareTitle(a, b) {
+    if (a.title < b.title) {
+        return -1;
+    }
+    if (a.title > b.title) {
+        return 1;
+    }
+    return 0;
+}
+
+function compareGenre(a, b) {
+    if (a.genre < b.genre) {
+        return -1;
+    }
+    if (a.genre > b.genre) {
+        return 1;
+    }
+    return 0;
+}
+
+function compareRating(a, b) {
+    if (a.rating < b.rating) {
+        return -1;
+    }
+    if (a.rating > b.rating) {
+        return 1;
+    }
+    return 0;
+}
+
+// function search(a) {
+//     if ()
+// }
+//
+// // create an array
+// const names = ['John', 'Peter', 'James', 'Pammy'];
+//
+// // Filter the array for names having 'am'
+// const myName = names.filter(name => name.includes('am'));
+//
+// // Output new array
+// console.log(myName)
